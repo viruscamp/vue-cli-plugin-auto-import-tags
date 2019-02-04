cd5c1d300d7479ddb6babf07e63d320a007259ea
VirusCamp <viruscamp@gmail.com>
2019-02-04 20:32:20 +0800

    Add babel-loader for the generated source to fix #2

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ module.exports = (api, projectOptions) => {
   api.chainWebpack(webpackConfig => {
     webpackConfig.module
       .rule('vue')
-      .use('vue-loader')
-        .loader('vue-loader')
-        .tap(options => {
-          options.compiler = createVueTemplateCompilerWrapper(options.compiler)
-          return options
-        })
+        .use('vue-loader')
+          .loader('vue-loader')
+          .tap(options => {
+            options.compiler = createVueTemplateCompilerWrapper(options.compiler)
+            return options
+          })
 
     webpackConfig.module
-      .rule('auto-import-tag')
+      .rule('vue-custom-block-auto-import-tag')
         .resourceQuery(/blockType=auto-import-tag/)
-        .use('vue-loader')
+        .use('babel-loader')
+          .loader('babel-loader')
+          .end()
+        .use('auto-import-tag')
           .loader(require.resolve('./custom-block-loaders/auto-import-tag'))
           .end()
   })
